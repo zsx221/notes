caa12aca81bffd1d222b7419eb1e3262a3089349
xhd <1365821818@qq.com>
2022-10-06 13:41:03 +0800

    AOS解读

--- test/src/main/java/locks/ReentrantTest.java
+++ test/src/main/java/locks/ReentrantTest.java
@@ -1,6 +1,11 @@
 package locks;
+import org.junit.Test;
+
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ReentrantTest implements Runnable {
 
     public synchronized void get() {
@@ -21,5 +26,28 @@ public class ReentrantTest implements Runnable {
         for(int i=0;i<10;i++){
             new Thread(rt).start();
         }
+    }
+    @Test
+    public void Aqstest(){
+        ReentrantLock lock=new ReentrantLock();
+        Object o=new Object();
+        new Thread(()->{
+            lock.lock();
+            try {
+                o.wait(3l);
+                o.wait(3l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("上锁");
+            lock.lock();
+            System.out.println("再次上锁");
+        },"a").start();
+        new Thread(()->{
+            lock.unlock();
+            o.notify();
+            System.out.println("解锁成功");
+        },"c").start();
+        System.out.println("解锁成功锁");
     }
 }
