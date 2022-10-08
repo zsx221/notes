f0f864af6f3f769e7948bf5f7416fab631efda1b
Xhd <1365821818@qq.com>
2022-10-08 18:02:18 +0800

    更新笔记

--- test/src/main/java/HashMapTest/HashMapTest.java
+++ test/src/main/java/HashMapTest/HashMapTest.java
@@ -1,7 +1,14 @@
 package HashMapTest;
 
-import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
+import java.util.HashMap;
+@Slf4j
 public class HashMapTest {
     HashMap<String,String>map=new HashMap<>();
+    @Test
+    public void tesr1(){
+        log.info("");
+    }
 }
