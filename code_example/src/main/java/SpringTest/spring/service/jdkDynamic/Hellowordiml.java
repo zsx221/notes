7b5fbc64096bbf81c1c4fea47a2ef8e77cbe846b
xhd <1365821818@qq.com>
2024-02-25 11:06:09 +0800

    预习面经

--- code_example/src/main/java/SpringTest/spring/service/jdkDynamic/Hellowordiml.java
+++ code_example/src/main/java/SpringTest/spring/service/jdkDynamic/Hellowordiml.java
@@ -1,12 +1,12 @@
 package SpringTest.spring.service.jdkDynamic;
 
-public class Hellowordiml implements helloword{
+public class Hellowordiml implements helloword {
     @Override
     public void sayhello() {
         System.out.println("hello world");
     }
 
     public static void main(String[] args) {
-        System.out.println(3*0.1);
+        System.out.println(3 * 0.1);
     }
 }
